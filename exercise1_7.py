3a2802b56dfe5d42e96dc83de48db7f273c54c03
kunyoungPark <p3145@daum.net>
2021-03-26 14:42:29 +0900

    Create exercise1_7.py

---
+++ exercise1_7.py
@@ -0,0 +1,14 @@
+thislist = ["apple","banana","cherry"]
+print("origin: ",thislist)
+
+#1.7.1 change the second item
+thislist [1] = "orange"
+print("1.7.1: ",thislist)
+
+#1.7.2 insert "melon"
+thislist.append("melon")
+print("1.7.2: ",thislist)
+
+#1.7.3 insert "mango" as the third item
+thislist.insert(2,"mango")
+print("1.7.3: ",thislist)
