931c46daaf9314723e01880a6c4e144ee6612a48
klee2025 <81403550+klee2025@users.noreply.github.com>
2021-03-27 13:42:16 +0900

    Create exercise1_10.py

---
+++ exercise1_10.py
@@ -0,0 +1,12 @@
+for x in range (1,10):
+    print("<[",end='')
+    for y in range (1,x):
+        print (y,end='')
+    print ("]>")
+
+
+for x in range (1,9):
+    print("<[",end='')
+    for y in range (x,9):
+        print(y, end='')
+    print("]>")
