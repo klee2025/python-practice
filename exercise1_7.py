70c2ff8df01ced0d5f41d4cbb11f9e868186053d
klee2025 <81403550+klee2025@users.noreply.github.com>
2021-03-27 11:38:16 +0900

    Update exercise1_7.py

--- exercise1_7.py
+++ exercise1_7.py
@@ -12,3 +12,8 @@ print("1.7.2: ",thislist)
 #1.7.3 insert "mango" as the third item
 thislist.insert(2,"mango")
 print("1.7.3: ",thislist)
+
+#1.7.4 consider thislist = ["apple","banana","cherry]. You can delete the last item in 3 ways below.
+thislist.remove("cherry")
+thislist.pop()
+del thislist[2]
