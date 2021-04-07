2176e1bac8518630dcbbf0fe40871cb1b44d5d54
klee2025 <81403550+klee2025@users.noreply.github.com>
2021-04-07 15:13:06 +0900

    Update exercise2_5.py

--- exercise2_5.py
+++ exercise2_5.py
@@ -3,4 +3,17 @@ x=1
 for x in range (1,31):
     if x % 3==0:
         continue
-    print(x)
+    print(x) 
+
+#2.5.3 Consider this dna:
+#2.5.3.a Store this dna string in a list.
+dna = 'gcatgacgttattacgactctgtcacgccgcggtgcgactgaggcgtggcgtctgctgggcctttacttcgcctccgcgccctgcattccgttcctggcctcg'
+
+#2.5.3.b 각 염기의 개수를 dictionary형태로 저장하세요. Ex: {‘g’:30}
+dna.count('g')
+dna.count('c')
+dna.count('a')
+dna.count('t')
+thisdict = {'g':30,'c':36,'a':10,'t':27}
+
+#2.5.3.c Draw a graph with values in the dictionary
